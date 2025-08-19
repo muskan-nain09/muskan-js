d3fb878a0b5eca9f3e87e3152f5e1ace852339e9
Muskan Nain <nainmuskan03@gmail.com>
2025-08-19 11:26:35 +0000

    strings changes

--- basics_1/strings.js
+++ basics_1/strings.js
@@ -11,8 +11,18 @@ const Naam = new String("muskanxd")
 // console.log(Naam.charAt(3))
 // console.log(Naam.indexOf("n"))
 
-let Naam2 = Naam.substring(0,3) // isme sirf positive no ho skte h 
-console.log(Naam2)
+// let Naam2 = Naam.substring(0,3) // isme sirf positive no ho skte h 
+// console.log(Naam2)
 
-// let Naam2 = Naam.slice(-6,2)// isme negative bhi hoskte h and reverse way m return krdega ye value
-// console.log(Naam2); // iska result = naku 
+let Naam2 = Naam.slice(-6,2)// isme negative bhi hoskte h and reverse way m return krdega ye value
+console.log(Naam2); // iska result = naku 
+
+// let Naam3 = "    muskaaan   "  //  trim extra spaces
+// console.log(Naam3)
+// console.log(Naam3.trim())
+
+const url = "https://muskan_nainxd%%20nainmuska"
+console.log(url.replace("%%", "-"))
+console.log(url.includes("muskan_nain"))
+
+console.log(url.split("%")) // iska syntax hoga split("separator", "limit")
