b5a016ed2c64328b42236552411e230c7bc87614
Muskan Nain <nainmuskan03@gmail.com>
2025-08-19 09:56:07 +0000

    comparison

---
+++ basics_1/comparison.js
@@ -0,0 +1,18 @@
+//same data type should be compared
+// console.log(2>1)
+// console.log("2">1)
+// console.log("2"<1)
+// console.log(2>=1)
+// console.log(2<=1)
+// console.log("2" != 1)
+// console.log("2"==1) 
+
+// string wale m ye conversion krke compare krta h 
+
+console.log(null > 0) // return false kyunki har baar null ko koi or value assign hoti h 
+console.log(null == 0) // return false
+console.log(null >= 0) // return true
+
+// for case of undefined it returns false for every case  .......THAT'S WHY SAME DATATYPES SHOULD BE COMPARED
+
+// === triple equal not only check value but also the datatype
--- basics_1/datatypes.js
+++ basics_1/datatypes.js
@@ -22,4 +22,35 @@ let name;
 // console.log(typeof "muskan")
 // console.log(typeof age)
 // console.log(typeof undefined) // return undefined for undefined type
-console.log(typeof null)  // object return krta h null k type ka 
+// console.log(typeof null)  // object return krta h null k type ka 
+
+// **************************************** OPERATIONS ************************************ 
+
+let value= 3
+let negative  = -value
+// console.log(negative)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2) //power of 2 
+// console.log(2/3)
+// console.log(2%3) 
+
+let str1 = "hello"
+let str2 = " world"
+let str3  =  str1+ str2
+console.log(str3) // hello world
+
+console.log("1"+2) //12
+console.log(1+"2") //12
+console.log("1"+"2")//12
+console.log("1"+2+2) // 122 
+console.log(1+2+"2") // 32 print krega // sab norms hote h we can't change these norms (ecma script me)
+
+// use parenthesis as much as possible
+
+// bad practice - console.log(true) -- it will return true
+// console.log(+true) = 1
+//console.log(true+) = error
+//console.log(+"") = empty string false dega to output  = 0
