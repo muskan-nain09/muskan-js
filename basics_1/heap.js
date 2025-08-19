7ac0f6e5ce89ddd69e78c1c9a6d04f3fe3b4a571
Muskan Nain <nainmuskan03@gmail.com>
2025-08-19 10:38:40 +0000

    heaps

---
+++ basics_1/heap.js
@@ -0,0 +1,16 @@
+// let name_ ="Muskan Nain"
+// let anothername = name_
+// anothername = "pikachu"
+// console.log(name_);
+// console.log(anothername);
+
+let user1 = {
+    Name : "Muskan__xd",
+    age : 21
+}
+
+let user2 = user1
+
+user2.Name = "pikachu"
+console.log(user2.Name);
+console.log(user1.Name)
