39d8a8fe864ef810426cd10237d88bdfe389cfc4
Muskan Nain <nainmuskan03@gmail.com>
2025-08-22 06:55:07 +0000

    switch

---
+++ basics_4/switch.js
@@ -0,0 +1,23 @@
+month = 3 // or agar ye string h like "march" to one ki jgh "march" ko krdo and then console.log
+switch(month){
+    case 1:
+        console.log("january")
+        break;
+    
+    case 2:
+        console.log("february")
+        break;
+
+     case 3:
+        console.log("march")
+        break; //  it breaks the current iteration
+    
+     case 4:
+        console.log("april")
+        break;
+    
+    default:
+        console.log("Defalut hai")
+        break;
+    
+}
