147b0235add78ad7d0f11f781bc19dfa3899d174
Muskan Nain <nainmuskan03@gmail.com>
2025-08-22 06:44:32 +0000

    if else

---
+++ basics_4/first.js
@@ -0,0 +1,21 @@
+// const temp = 40
+
+// if(temp==50){
+//     console.log("Temperature is 50")
+// }
+// else if (temp>50){
+//     console.log("Temperature is greater than 50")
+// }
+// else{
+//     console.log("pta nahi temperature shayad 50 se kam h")
+// }
+
+// const balance =1000
+// if(balance>100) console.log("bhot paisa h") // ye aap direct bhi kr skte h  but not impressive at all 
+
+const user =true
+const card = true
+const money = false
+if(user &&  card){ // or ka bhi use krte h using '||' 
+    console.log("u can buy")
+}
